f300028f0cf9127dfafaab14bcbbab1836910023
shreyachak1712 <shreyachakraborty1712@gmail.com>
2024-04-03 12:44:40 +0530

    Login

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:road_rescue_app/main.dart';
+import 'package:road_rescue_app/login.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
