c38e74e2d1e7e1c1e7d41dab6039bc86a01f9c80
DwayneNixon <dwaynenixon2004@gmail.com>
2024-04-04 08:15:50 +0530

    Added Firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:road_rescue_app/garage.dart';
 import 'package:road_rescue_app/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
